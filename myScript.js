35fd8225213c3aa3ac4936036ecdbde0b63eb46e
Moses Palacios <mospalacios@gmail.com>
2020-01-04 18:24:49 -0500

    changed api url to begin with https

--- myScript.js
+++ myScript.js
@@ -4,9 +4,9 @@ var cities
 var currentWeatherEl = $("#currentWeatherInfo");
 var forecastEl = $("#forecastInfo");
 
-var currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?";
-var uvIndexUrl = "http://api.openweathermap.org/data/2.5/uvi?";
-var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?";
+var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?";
+var uvIndexUrl = "https://api.openweathermap.org/data/2.5/uvi?";
+var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?";
 
 var currentWeatherParam = {"APPID" : "20912c814361943ea6f62b43e249f46f"};
 var uvIndexParam = {"APPID" : "20912c814361943ea6f62b43e249f46f"};
@@ -193,58 +193,4 @@ $(".pastCities").on("click", function(){
 })
 
 })
-        // then(function(response) {
-        // lat = response.coord.lat;
-        // lon = response.coord.lon;
-
-        // var uvURL = buildUvURL();
-        // var fiveDayURL = buildforecastURL();
-
-        // console.log(response);
         
-
-        // var currentWeatherHeader = $("<h2>").text(response.name + " " + moment.unix(response.dt).format("M/D/YYYY"));
-        // var weathericon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png")
-        // var currentWeatherTemp = $("<p>").text("Temperature: " + response.main.temp + " F");
-        // var currentWeatherHumid = $("<p>").text("Humidity: " + response.main.humidity + "%");
-        // var currentWeatherWind = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
-        // //currentWeatherEl.append("<h2>" +  response.name + " " + moment.unix(response.dt).format("M/D/YYYY") + "<img src= http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png>" +"</h2>");
-        // currentWeatherHeader.append(weathericon);
-        // currentWeatherEl.append(currentWeatherHeader, currentWeatherTemp, currentWeatherHumid, currentWeatherWind)
-
-        //     $.ajax({
-        //             url: uvURL,
-        //             method: "GET"
-        //             }).then(function(uvResponse) {
-        //             var uvIndexP = $("<p>").text("UV Index: ");
-        //             var uvIndexVal = $("<span>").addClass("btn btn-danger").text(uvResponse.value);
-        //             uvIndexP.append(uvIndexVal);
-        //             currentWeatherEl.append(uvIndexP)
-
-        //             })
-        // $.ajax({
-        //     url: fiveDayURL,
-        //     method: "GET"
-        //     }).then(function(fiveDayResponse) {
-        //         console.log(fiveDayResponse.list);
-
-        //         forecastEl.append($("<h2>").addClass("col-md-12").text("5 Day Forecast"));
-
-        //         for(i = 3; i < fiveDayResponse.list.length; i+=8){
-        //             var newDiv = $("<div>").addClass("col-md-2 rounded bg-primary mx-2 pt-2 text-left");
-        //             var newDate = $("<h6>").text(moment.unix(fiveDayResponse.list[i].dt).format("M/D/YYYY"));
-        //             var newIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + fiveDayResponse.list[i].weather[0].icon + ".png");
-        //             var newTemp = $("<p>").text("Temperature: " + fiveDayResponse.list[i].main.temp + " F");
-        //             var newHumid = $("<p>").text("Temperature: " + fiveDayResponse.list[i].main.humidity + "%");
-
-        //             newDiv.append(newDate, newIcon, newTemp, newHumid);
-        //             forecastEl.append(newDiv)
-
-        //         }
-        //     })
-
-        // });
-
-     
-   
-    
